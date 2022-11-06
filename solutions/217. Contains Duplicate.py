962d8951429da264f3291ee8f420211015a2fac0
hotsno <71658949+hotsno@users.noreply.github.com>
2022-11-06 13:37:37 -0800

    Remove '217. Contains Duplicate.py'

--- solutions/217. Contains Duplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        s = set()
-        for num in nums:
-            if num in s:
-                return True
-            s.add(num)
-        return False
