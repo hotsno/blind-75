0d9a00d954a8f2325d2095ceaafbac32efe27390
hotsno <71658949+hotsno@users.noreply.github.com>
2022-11-10 10:29:44 -0800

    Add '23. Merge k Sorted Lists.py'

---
+++ solutions/23. Merge k Sorted Lists.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        while len(lists) != 1:
+            merged = []
+            i = 0
+            while i < len(lists):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                merge = self.mergeLists(l1, l2)
+                merged.append(merge)
+            lists = merged
+
+    def mergeLists(self, l1, l2):
+        dummy = cur = ListNode()
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+            else:
+                cur.next = l2
+            cur = cur.next
+        if l1:
+            cur.next = l1
+        else:
+            cur.next = l2
+        return dummy.next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeLists(self, l1, l2):
+        dummy = cur = ListNode()
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        if l1:
+            cur.next = l1
+        else:
+            cur.next = l2
+        return dummy.next
+
