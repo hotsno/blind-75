d460368ed23ce331841e6e186112f032e28662e6
hotsno <71658949+hotsno@users.noreply.github.com>
2022-11-14 23:14:35 -0800

    Modify '70. Climbing Stairs.py'

--- solutions/70. Climbing Stairs.py
+++ solutions/70. Climbing Stairs.py
@@ -2,8 +2,9 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
-        dp = [0] * n
-        dp[n - 1], dp[n - 2] = 1, 2
+        n1, n2 = 1, 2
         for i in range(n - 3, -1, -1):
-            dp[i] = dp[i + 1] + dp[i + 2]
-        return dp[0]
+            temp = n1
+            n1 = n2
+            n2 = n2 + temp
+        return n2
